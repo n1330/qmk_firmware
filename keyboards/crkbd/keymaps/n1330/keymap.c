31ec676437bd2dded7a660bb9876e96d9dcf3226
Tomohiro Nagai <n1330@me.com>
2022-08-15 10:30:13 +0900

    .

--- keyboards/crkbd/keymaps/n1330/keymap.c
+++ keyboards/crkbd/keymaps/n1330/keymap.c
@@ -61,7 +61,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
   [_LOWER] = LAYOUT_split_3x6_3(
   //,-----------------------------------------------------.                    ,-----------------------------------------------------.
-      XXXXXXX, XXXXXXX, KC_AMPR, KC_PIPE, KC_TILD, KC_EXLM,                      KC_MINS, KC_PLUS,  KC_EQL, KC_ASTR, KC_PERC, XXXXXXX,
+      XXXXXXX, XXXXXXX, KC_AMPR, KC_PIPE, KC_TILD, KC_EXLM,                      KC_PLUS,  KC_EQL, KC_MINS, KC_ASTR, KC_PERC, XXXXXXX,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
       XXXXXXX,    KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                         KC_6,    KC_7,    KC_8,    KC_9,    KC_0, XXXXXXX,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
@@ -73,7 +73,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
   [_RAISE] = LAYOUT_split_3x6_3(
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
-      XXXXXXX, XXXXXXX, KC_AMPR, KC_PIPE, KC_TILD, KC_EXLM,                      KC_MINS, KC_PLUS,  KC_EQL, KC_ASTR, KC_PERC, XXXXXXX,
+      XXXXXXX, XXXXXXX, KC_AMPR, KC_PIPE, KC_TILD, KC_EXLM,                      KC_PLUS,  KC_EQL, KC_MINS, KC_ASTR, KC_PERC, XXXXXXX,
   //,-----------------------------------------------------.                    ,-----------------------------------------------------.
       XXXXXXX, KC_HASH,  KC_GRV,  KC_DQT, KC_QUOT, KC_UNDS,                      KC_LEFT, KC_DOWN,   KC_UP,KC_RIGHT,  KC_ESC, XXXXXXX,
   //|--------+--------+--------+--------+--------+--------|                    |--------+--------+--------+--------+--------+--------|
